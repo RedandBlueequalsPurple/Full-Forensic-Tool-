f94c9e89d7b89aeab3e7155d64b5d5d6a78ae399
oriraz <mymac@Oris-MacBook-Pro.local>
2024-07-31 22:29:04 +0300

    five commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import subprocess
+import os 
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+#if __name__ == '__main__':
+
+print(" Choose which tool you need:")
+ListOfTools = [[1, 'Email'],[2, 'PDF'],[3, 'ISO'],[4, 'OVA'],[5, 'URL'],[6, 'JSON'],[7, 'EVENT VIEWER'],[8, 'PNG'],[9, 'CODE'],[10, 'EXE / DMG']]
+for i in ListOfTools:
+    print(i[0], i[1])
+
+while True:
+    Choice = input("Choose the tool you need or type 'exit' to quit: ")
+    if Choice == 'exit' :
+        print("Bye!")
+        break
+    elif Choice == "1" :
+        print("Email Analysis was selected")
+        import Tools.Email_Analysis as Email_Analysis
+        Email_Analysis.main()
+        break
+    elif Choice == "2" :
+        print("PDF Analysis was selected")
+        import Tools.PDF_Analysis as PDF_Analysis
+        PDF_Analysis.main()
+        break
+    elif Choice == "3" :
+        print("ISO Analysis was selected")
+        import Tools.ISO_Analysis as ISO_Analysis
+        ISO_Analysis.main()
+        break
+    elif Choice == "4" :
+        print("OVA Analysis was selected")
+        import Tools.OVA_Analysis as OVA_Analysis
+        OVA_Analysis.main()
+        break
+    elif Choice == "5" :
+        print("URL Analysis was selected")
+        import Tools.URL_Analysis as URL_Analysis
+        URL_Analysis.main()
+        break
+    elif Choice == "6" :
+        print("JSON Analysis was selected")
+        import Tools.JSON_Analysis as JSON_Analysis
+        JSON_Analysis.main()
+        break
+    elif Choice == "7" :
+        print("Email Analysis was selected")
+        import Tools.Email_Analysis as Email_Analysis
+        Email_Analysis.main()
+        break
+    elif Choice == "8":
+        print("OVA Analysis was selected")
+        import Tools.OVA_Analysis as OVA_Analysis
+        OVA_Analysis.main()
+        break
+    elif Choice == "9":
+        print("URL Analysis was selected")
+        import Tools.URL_Analysis as URL_Analysis
+        URL_Analysis.main()
+        break
+    elif Choice == "10":
+        print("URL Analysis was selected")
+        import Tools.URL_Analysis as URL_Analysis
+        URL_Analysis.main()
+        continue
