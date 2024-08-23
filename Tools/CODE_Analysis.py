76f0067022528cfd7bf0512181965b86d0a6cd13
Your Name <you@example.com>
2024-08-23 12:55:00 +0300

    update the log function

--- Tools/CODE_Analysis.py
+++ Tools/CODE_Analysis.py
@@ -1,5 +1,30 @@
+import logging
+
+# Configure logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# File handler
+file_handler = logging.FileHandler('event_history.log')
+file_handler.setLevel(logging.DEBUG)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+# Formatter
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
 def main():
-    print("STAY TUNE, WILL BE ADDED IN THE FUTURE")
+    message = "STAY TUNE, WILL BE ADDED IN THE FUTURE"
+    print(message)
+    logger.info(message)
 
 if __name__ == "__main__":
     main()
